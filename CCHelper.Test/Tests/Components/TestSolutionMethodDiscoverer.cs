1313802062887a658dfcc3a97da9682fc3bd47b4
GualaBanana <tem4rik@gmail.com>
2022-03-09 15:54:16 +0300

    Add tests for SolutionMethodDiscoverer.

---
+++ CCHelper.Test/Tests/Components/TestSolutionMethodDiscoverer.cs
@@ -0,0 +1,58 @@
+﻿using CCHelper.Services;
+using CCHelper.Test.Framework;
+using CCHelper.Test.Framework.Abstractions.SolutionContext;
+using CCHelper.Test.Framework.Abstractions.SolutionMethod;
+using CCHelper.Test.Framework.TestData;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace CCHelper.Test.Tests.Components;
+
+public class TestSolutionMethodDiscoverer : DynamicContextFixture
+{
+    Action SUT_SearchSolutionContainer<TResult>(TResult _)
+    {
+        return () => SolutionMethodDiscoverer.SearchSolutionContainer<TResult>(_context.SolutionContainer.Instance);
+    }
+
+    [Fact]
+    public void ShouldNotThrow_WhenSolutionMethodIsDiscovered()
+    {
+        SolutionMethodStub
+            .NewStub
+            .WithSolutionLabel
+            .Returning(TypeData.DummyType)
+            .PutInContext(_context);
+
+        Assert.True(SUT_SearchSolutionContainer(TypeData.DummyValue).DoesNotThrow());
+    }
+
+    [Fact]
+    public void ShouldThrowEntryPointNotFoundException_WhenSolutionContainerHasNoSolutionMethods()
+    {
+        SolutionMethodStub
+            .NewStub
+            .Returning(TypeData.DummyType)
+            .PutInContext(_context);
+
+        Assert.Throws<EntryPointNotFoundException>(SUT_SearchSolutionContainer(TypeData.DummyType));
+    }
+
+    [Fact]
+    public void ShouldThrowAmbiguousMatchException_WhenSolutionContainerHasMultipleSolutionMethods()
+    {
+        SolutionMethodStub
+            .NewStub
+            .WithSolutionLabel
+            .Returning(TypeData.DummyType)
+            .PutInContext(_context);
+        SolutionMethodStub
+            .NewStub
+            .Accepting(TypeData.DummyType)
+            .WithResultLabelAppliedToParameter(1)
+            .PutInContext(_context);
+
+        Assert.Throws<AmbiguousMatchException>(SUT_SearchSolutionContainer(TypeData.DummyType));
+    }
+}
